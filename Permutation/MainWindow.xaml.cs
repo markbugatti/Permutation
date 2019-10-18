95c378d4ab0874f7e7f8db47b2c05aec66c26e83
Mark <supermark11@gmail.com>
2019-10-18 21:00:53 +0300

    вынесены основные функции по заполнению таблицы в TableHandler

--- Permutation/MainWindow.xaml.cs
+++ Permutation/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Permutation
     public partial class MainWindow : Window
     {
         private const int UniformLength = 1; // Border Thickness
-        private const int size = 40; // column and row size at the table
+        
         // глобальные статичиские перменные, хранят ссылки на таблицы из XAML
         static public Grid OriginalTable { get; private set; }
         static public Grid EncryptedTable { get; private set; }
@@ -30,6 +30,7 @@ namespace Permutation
         public int keyWordLength;
         public int OriginalTextLength;
         public string keyWord;
+        public int[] keyWordIndexes;
         public string OriginalText;
         public MainWindow()
         {
@@ -57,74 +58,20 @@ namespace Permutation
                 }
                 // Добавляем 2 рядка для секретного слова и индекса
                 rowCount += 2;
-
-                for (int i = 0; i < columnCount; i++)
-                {
-                    ColumnDefinition column = new ColumnDefinition();
-                    column.Width = new GridLength(size);
-                    OriginalTable.ColumnDefinitions.Add(column);
-                    column = new ColumnDefinition();
-                    column.Width = new GridLength(size);
-                    EncryptedTable.ColumnDefinitions.Add(column);
-                }
                 
-                for (int i = 0; i < rowCount; i++)
-                {
-                    RowDefinition row = new RowDefinition();
-                    row.Height = new GridLength(size);
-                    OriginalTable.RowDefinitions.Add(row);
-                    row = new RowDefinition();
-                    row.Height = new GridLength(size);
-                    EncryptedTable.RowDefinitions.Add(row);
-                }
+                //  Добавить в таблицу колонки
+                TableHandler.AddColumns(columnCount);
+                // добавить в таблицу рядочки
+                TableHandler.AddRows((int)rowCount);
+                
                 OriginalText = OriginalTextBox.Text;
                 keyWord = KeyTextBox.Text;
 
-                string text; // представляет символ, который будет записан в клеточку
-                // первый и второй ряд - секретное слово и индекс
-                for (int i = 0; i < columnCount; i++)
-                {
-                    // Text
-                    text = keyWord[i].ToString();
-                    // Grid
-                    SquareCreator.create(text, 0, i);
-                    
-                    // Text
-                    int num = (int)keyWord[i];
-                    text = num.ToString();
-                    // Grid
-                    SquareCreator.create(text, 1, i);
-                }
+                TableHandler.FillKeyWord(keyWord, columnCount);
+                TableHandler.FillText(OriginalText, (int)rowCount, columnCount);
 
-                // начиная со второго рядка заполнять теблицу символами исходного текста
-                int index = 0;
-                int indexExtra = 97; // индекс для екстра символов. Для заполнение пустых клеточек
-                int counterExtra = 0; // Показывает сколько екстра символов было добавлено
-                
-                for (int i = 2; i < rowCount; i++)
-                {
-                    for (int j = 0; j < columnCount; j++)
-                    {
-                        // Text 1.1
-                        if (index < OriginalText.Length) {
-                            text = OriginalText[index++].ToString();
-                        }
-                        else
-                        {
-                            char symbol = (char)indexExtra;
-                            text = symbol.ToString();
-                            indexExtra++;
-                            counterExtra++;
-                        }
-                        // Grid
-                        SquareCreator.create(text, i, j);
-                        
-                    }
-                }
-                text = counterExtra.ToString();
-                SquareCreator.create(text, (int)rowCount - 1, columnCount - 1);
-                //OriginalTable.SetValue(Grid.ShowGridLinesProperty, true);
-                //EncryptedTable.SetValue(Grid.ShowGridLinesProperty, true);
+                //
+
             }
         }
     }
---
+++ Permutation/TableHandler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Permutation
+{
+    class TableHandler
+    {
+        private const int size = 40; // column and row size at the table
+        public static TextBlock Find(int row, int column)
+        {
+            foreach (Border border in MainWindow.OriginalTable.Children)
+            {
+                int borderRow = (int)border.GetValue(Grid.RowProperty);
+                int borderColumn = (int)border.GetValue(Grid.ColumnProperty);
+                if (borderRow == row && borderColumn == column)
+                {
+                    TextBlock textBlock = (TextBlock)border.Child;
+                    return textBlock;
+                }
+            }
+            throw new Exception("can`t find TextBlock");
+        }
+
+        public static void AddColumns(int count)
+        {
+            //  Добавить в таблицу колонки
+            for (int i = 0; i < count; i++)
+            {
+                ColumnDefinition column = new ColumnDefinition();
+                column.Width = new GridLength(size);
+                MainWindow.OriginalTable.ColumnDefinitions.Add(column);
+                column = new ColumnDefinition();
+                column.Width = new GridLength(size);
+                MainWindow.EncryptedTable.ColumnDefinitions.Add(column);
+            }
+        }
+
+        public static void AddRows(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                RowDefinition row = new RowDefinition();
+                row.Height = new GridLength(size);
+                MainWindow.OriginalTable.RowDefinitions.Add(row);
+                row = new RowDefinition();
+                row.Height = new GridLength(size);
+                MainWindow.EncryptedTable.RowDefinitions.Add(row);
+            }
+        }
+
+        public static void FillKeyWord(string keyWord, int columnCount)
+        {
+            string text; // представляет символ, который будет записан в клеточку
+                         // первый и второй ряд - секретное слово и индекс
+            for (int i = 0; i < columnCount; i++)
+            {
+                // Text
+                text = keyWord[i].ToString();
+                // Grid
+                SquareCreator.create(text, 0, i);
+
+                // Text
+                int num = (int)keyWord[i];
+                text = num.ToString();
+                // Grid
+                SquareCreator.create(text, 1, i);
+            }
+        }
+
+        public static void FillText(string text, int rowCount, int columnCount)
+        {
+            // начиная со второго рядка заполнять таблицу символами исходного текста
+            int index = 0;
+            int indexExtra = 97; // индекс для екстра символов. Для заполнение пустых клеточек
+            int counterExtra = 0; // Показывает сколько екстра символов было добавлено
+            string character;
+            
+            for (int i = 2; i < (int)rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    // Text 1.1
+                    if (index < text.Length)
+                    {
+                        character = text[index++].ToString();
+                    }
+                    else
+                    {
+                        char a = (char)indexExtra;
+                        character = a.ToString();
+                        indexExtra++;
+                        counterExtra++;
+                    }
+                    // Grid
+                    SquareCreator.create(character, i, j);
+                }
+            }
+
+            character = counterExtra.ToString();
+            TextBlock textBlock = Find((int)rowCount - 1, columnCount - 1);
+            textBlock.Text = "";
+            SquareCreator.create(text, (int)rowCount - 1, columnCount - 1);
+        }
+    }
+}
